95dcd81fdf4cd5b36e9b3292f8e6d1c9347bee7f
Vanshika Kejriwal <vanshikak64@gmail.com>
2020-08-08 21:32:27 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,5 @@
+radius = int(input("input the radius of circle: "))  
+area = 2*22/7*radius
+print("The area of circle with radius " + str(radius) + " is: " + str(area))
+file_name=input("input the file name")
+print("the extension of the file is: python")
